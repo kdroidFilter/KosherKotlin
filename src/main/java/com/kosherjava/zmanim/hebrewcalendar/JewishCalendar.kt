8cc72bc770c3c6401965129f9446415a27fc581b
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-07-11 13:01:35 +0300

    Add unused code (usually not a good idea...)

--- src/main/java/com/kosherjava/zmanim/hebrewcalendar/JewishCalendar.kt
+++ src/main/java/com/kosherjava/zmanim/hebrewcalendar/JewishCalendar.kt
@@ -1891,6 +1891,47 @@ class JewishCalendar : JewishDate {
          */
         const val BEHAB = 37
 
+        /* maybe this will be useful some day...
+        val holidays = intArrayOf(
+            EREV_PESACH,
+            PESACH,
+            CHOL_HAMOED_PESACH,
+            PESACH_SHENI,
+            EREV_SHAVUOS,
+            SHAVUOS,
+            SEVENTEEN_OF_TAMMUZ,
+            TISHA_BEAV,
+            TU_BEAV,
+            EREV_ROSH_HASHANA,
+            ROSH_HASHANA,
+            FAST_OF_GEDALYAH,
+            EREV_YOM_KIPPUR,
+            YOM_KIPPUR,
+            EREV_SUCCOS,
+            SUCCOS,
+            CHOL_HAMOED_SUCCOS,
+            HOSHANA_RABBA,
+            SHEMINI_ATZERES,
+            SIMCHAS_TORAH,
+            CHANUKAH,
+            TENTH_OF_TEVES,
+            TU_BESHVAT,
+            FAST_OF_ESTHER,
+            PURIM,
+            SHUSHAN_PURIM,
+            PURIM_KATAN,
+            ROSH_CHODESH,
+            YOM_HASHOAH,
+            YOM_HAZIKARON,
+            YOM_HAATZMAUT,
+            YOM_YERUSHALAYIM,
+            LAG_BAOMER,
+            SHUSHAN_PURIM_KATAN,
+            ISRU_CHAG,
+            YOM_KIPPUR_KATAN,
+            BEHAB,
+        )*/
+
         /**
          * An array of *parshiyos* in the 17 possible combinations.
          */
