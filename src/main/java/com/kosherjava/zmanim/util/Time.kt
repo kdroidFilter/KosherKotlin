335eee0e4b0b1eb08e24348c18aac575195c119e
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-07-11 16:58:31 +0300

    Make Time more idiomatic

--- src/main/java/com/kosherjava/zmanim/util/Time.kt
+++ src/main/java/com/kosherjava/zmanim/util/Time.kt
@@ -15,114 +15,53 @@
  */
 package com.kosherjava.zmanim.util
 
-import com.kosherjava.zmanim.AstronomicalCalendar
 import java.util.*
+import kotlin.math.abs
 
 /**
  * A class that represents a numeric time. Times that represent a time of day are stored as [java.util.Date]s in
  * this API. The time class is used to represent numeric time such as the time in hours, minutes, seconds and
  * milliseconds of a [temporal hour][com.kosherjava.zmanim.AstronomicalCalendar.getTemporalHour].
  *
+ * This is a data class so that it inherits the [copy] method, and to semantically signify it is a data holder.
  * @author  Eliyahu Hershfeld 2004 - 2020
  */
-class Time {
-    /**
-     * @return Returns the hour.
-     */
-    /**
-     * @param hours
-     * The hours to set.
-     */
-    /**
-     * @see .getHours
-     */
-    var hours = 0
-    /**
-     * @return Returns the minutes.
-     */
-    /**
-     * @param minutes
-     * The minutes to set.
-     */
-    /**
-     * @see .getMinutes
-     */
-    var minutes = 0
-    /**
-     * @return Returns the seconds.
-     */
-    /**
-     * @param seconds
-     * The seconds to set.
-     */
-    /**
-     * @see .getSeconds
-     */
-    var seconds = 0
-    /**
-     * @return Returns the milliseconds.
-     */
-    /**
-     * @param milliseconds
-     * The milliseconds to set.
-     */
-    /**
-     * @see .getMilliseconds
-     */
-    var milliseconds = 0
+data class Time(
+    var hours: Int = 0,
+    var minutes: Int = 0,
+    var seconds: Int = 0,
+    var milliseconds: Int = 0,
     /**
      * Does the time represent a negative time 9such as using this to subtract time from another Time.
-     * @return if the time is negative.
+     * True if the time is negative.
      */
-    /**
-     * Set this to represent a negative time.
-     * @param isNegative that the Time represents negative time
-     */
-    /**
-     * @see .isNegative
-     * @see .setIsNegative
-     */
-    var isNegative = false
-
-    /**
-     * Constructor with parameters for the hours, minutes, seconds and millisecods.
-     *
-     * @param hours the hours to set
-     * @param minutes the minutes to set
-     * @param seconds the seconds to set
-     * @param milliseconds the milliseconds to set
-     */
-    constructor(hours: Int, minutes: Int, seconds: Int, milliseconds: Int) {
-        this.hours = hours
-        this.minutes = minutes
-        this.seconds = seconds
-        this.milliseconds = milliseconds
-    }
+    var isNegative: Boolean = false
+) {
 
     /**
      * Constructor with a parameter for milliseconds. This constructor casts the milliseconds to an int and
-     * calls [.Time]
+     * calls a secondary constructo
      * @param millis the milliseconds to set the object with.
      */
     constructor(millis: Double) : this(millis.toInt())
 
     /**
      * A constructor that sets the time by milliseconds. The milliseconds are converted to hours, minutes, seconds
-     * and milliseconds. If the milliseconds are negative it will call [isNegative].
+     * and milliseconds. If the milliseconds are negative it will set [isNegative] to true.
      * @param millis the milliseconds to set.
      */
-    constructor(millis: Int) {
+    constructor(millis: Int) : this() {
         var adjustedMillis = millis
         if (adjustedMillis < 0) {
             isNegative = true
-            adjustedMillis = Math.abs(adjustedMillis)
+            adjustedMillis = abs(adjustedMillis)
         }
         hours = adjustedMillis / HOUR_MILLIS
-        adjustedMillis = adjustedMillis - hours * HOUR_MILLIS
+        adjustedMillis -= hours * HOUR_MILLIS
         minutes = adjustedMillis / MINUTE_MILLIS
-        adjustedMillis = adjustedMillis - minutes * MINUTE_MILLIS
+        adjustedMillis -= minutes * MINUTE_MILLIS
         seconds = adjustedMillis / SECOND_MILLIS
-        adjustedMillis = adjustedMillis - seconds * SECOND_MILLIS
+        adjustedMillis -= seconds * SECOND_MILLIS
         milliseconds = adjustedMillis
     }
 
