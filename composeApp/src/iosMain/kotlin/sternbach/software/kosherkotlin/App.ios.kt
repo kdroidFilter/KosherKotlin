6aff528b6cf2ef6de7bab96c2ac33c4997904f26
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-10-31 13:51:16 -0400

    Remove openUrl

--- composeApp/src/iosMain/kotlin/sternbach/software/kosherkotlin/App.ios.kt
+++ composeApp/src/iosMain/kotlin/sternbach/software/kosherkotlin/App.ios.kt
@@ -3,7 +3,3 @@ package sternbach.software.kosherkotlin
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
 
-internal actual fun openUrl(url: String?) {
-    val nsUrl = url?.let { NSURL.URLWithString(it) } ?: return
-    UIApplication.sharedApplication.openURL(nsUrl)
-}
